6052111747010144ab219b2adee0b9cb034e1156
Kevin Yang <kevin.yang@elliemae.com>
2019-10-20 10:50:55 -0700

    initial commit

---
+++ src/ContentstackServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Elliemae\Contentstack;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContentstackServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+
+	}
+
+	public function register()
+	{
+
+	}
+}
